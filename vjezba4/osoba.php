d80a68e0ccfefb74af635c2cc4c8b79270c8af06
lcrljic <lukacrljic7@gmail.com>
2024-03-30 16:53:14 +0100

    vjezba 4

--- vjezba4/osoba.php
+++ vjezba4/osoba.php
@@ -46,14 +46,6 @@
 	</div><!-- end #divHeroSectionWrap -->
 	<div id="divSection1Wrap">
 		<div id="divSection1">
-			<div>
-				<?php 
-					$a = 5;
-					$b = 10;
-					$c = 10 + 15;
-					echo($c);
-				?>
-			</div>
 			<div class="divColWrap">
 				<div class="divCol6">
 					<div class="divContentBoxStyle1">
@@ -61,14 +53,6 @@
 							<img src="images/s1.png">
 						</div>
 						<div class="boxDescStyle1">
-							<?php  
-								$naslov = 'uslugaaaaa';
-								if($naslov=='usluga'){
-									echo '<h3>Neka moja usluga 11111</h3>';
-								}else{
-									echo '<h3>Neki moj proizvod</h3>';
-								}
-							?>
 							<p>Neki moj opis prve usluge koji ćemo kasnije prilagoditi</p>
 							<a href="#" class="buttonStyle2">Read more</a>
 						</div>
@@ -118,19 +102,74 @@
 			</div>
 		</div><!-- end #divSection1 -->
 	</div><!-- end #divSection1Wrap -->
+	<div id="divSection2Wrap">
+		<div id="divSection2">
+			<div class="divCol6">
+				<h3 class="headTitleStyle1"><span>O</span> nama</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
+				<a href="#" class="buttonStyle1">Više o nama</a>
+				<?php
+					if(isset($_REQUEST['btt1'])){
+						$servername = "localhost";
+						$username = "root";
+						$password = "";
+						$dbname = "edunovaw34";
+
+						// Create connection
+						$conn = new mysqli($servername, $username, $password, $dbname);
+						// Check connection
+						if ($conn->connect_error) {
+						  die("Connection failed: " . $conn->connect_error);
+						}
+
+						$ime = $_REQUEST['ime'];
+						$prezime = $_REQUEST['prezime'];
+						$oib = $_REQUEST['oib'];
+
+						$sql = "INSERT INTO osoba (ime, prezime, oib)
+						VALUES ('$ime', '$prezime', '$oib')";
+
+						if ($conn->query($sql) === TRUE) {
+						  echo "Nova osoba je unešena.";
+						} else {
+						  echo "Error: " . $sql . "<br>" . $conn->error;
+						}
+
+						$conn->close();
+					}
+				?>
+				<div id="divForm1">
+					<form id="obrazac1" method="post" action="">
+						<div class="divElementBox1">
+							<label for="ime">Ime</label>
+							<input id="ime" name="ime" type="text" value="" placeholder="Unesite svoje ime">
+						</div>
+						<div class="divElementBox1">
+							<label for="prezime">Prezime</label>
+							<input id="prezime" name="prezime" type="text" value="Neko prezime">
+						</div>
+						<div class="divElementBox1">
+							<label for="oib">OIB</label>
+							<input id="oib" name="oib" type="text" value="" placeholder="Unesite OIB">
+						</div>
+						<div class="divElementBox4">
+							<input id="btt1" name="btt1" type="submit" value="Spremi">
+							<input id="btt2" name="btt2" type="reset" value="Obriši">
+						</div>
+					</form>
+				</div>
+			</div>
+			<div class="divCol6 last">
+				<img src="images/about-img.jpg">
+			</div>
+			<div style="clear: both;"></div>
+		</div><!-- end #divSection2 -->
+	</div><!-- end #divSection2Wrap -->
 	<div id="divSection3Wrap">
 		<div id="divSection3">
-			<h3 class="headTitleStyle1"><span>G</span> alerija</h3>
-			<div class="divColWrap divGallery">
-				<?php 
-					for($i=1;$i<=4;$i++){
-				?>
-				<div class="divCol3 <?php if($i%4==0) echo 'last'; ?>">
-					<img src="images/team-<?php echo $i; ?>.jpg" novaputanja="images/testimonial-<?php echo $i; ?>.jpg">
-				</div>
-				<?php } ?>
-				<div style="clear: both;"></div>
-			</div>
 			<?php 
 				$team = array(
 					array('ime'=>'Pero','prezime'=>'Perić','oib'=>'12345678987'),
@@ -139,7 +178,7 @@
 					array('ime'=>'Miro','prezime'=>'Jozić','oib'=>'12047088987')
 				);
 			?>
-			<h3 class="headTitleStyle1"><span>G</span> alerija 2 - array foreach</h3>
+			<h3 class="headTitleStyle1">Osobe</h3>
 			<div class="divColWrap divGallery">
 				<?php 
 					$i=1;
@@ -159,150 +198,6 @@
 			</div>
 		</div>
 	</div>
-	<div id="divSection2Wrap">
-		<div id="divSection2">
-			<div class="divCol6">
-				<h3 class="headTitleStyle1"><span>O</span> nama</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
-				<a href="#" class="buttonStyle1">Više o nama</a>
-				<?php 
-				//ARRAY
-				$auti = array('BMW','Audi','Mercedes');
-				//echo $auti[1];
-				foreach($auti as $auto){
-					echo $auto.'<br>';
-				}
-				$osobe = array('osoba1'=>'Marko Marić','osoba2'=>'Pero Perić','osoba3'=>'Mirko Mirić');
-				//echo $osobe['osoba2'];
-				$ucionica = array(
-					array('ime'=>'Pero','prezime'=>'Perić','oib'=>'12345678987'),
-					array('ime'=>'Marko','prezime'=>'Marić','oib'=>'15681678987'),
-					array('ime'=>'Miro','prezime'=>'Jozić','oib'=>'12047088987'),
-					//'Mario Mijić',
-					//'polaznik'=>'Jozo Jozić',
-					//'Zvonko Zvonić'
-				);
-				echo $ucionica[1]['ime'].' - '.$ucionica[1]['prezime'].'<br>';
-				//FOREACH
-				foreach($ucionica as $polaznik){
-					echo $polaznik['ime'].'<br>';
-				}
-				echo '<pre>';
-				print_r($ucionica);
-				echo '</pre>';
-				// POST GET Submit form
-					if(isset($_REQUEST['btt1'])){
-						$ime = $_REQUEST['ime'];
-						$prezime = $_REQUEST['prezime'];
-						$mail = $_REQUEST['email'];
-						$poruka = $_REQUEST['poruka'];
-						echo '<h2>Vaše ime: '.$ime.' '.$prezime.'</h2>';
-						echo '<pre>';
-						print_r($_REQUEST);
-						echo '</pre>';
-						if($mail!=''){
-							mail($mail,'Moja poruka',$poruka);
-						}
-					}
-				?>
-				<div id="divForm1">
-					<form id="obrazac1" method="post" action="">
-						<div class="divElementBox1">
-							<label for="ime">Ime</label>
-							<input id="ime" name="ime" type="text" value="" placeholder="Unesite svoje ime">
-						</div>
-						<div class="divElementBox1">
-							<label for="prezime">Prezime</label>
-							<input id="prezime" name="prezime" type="text" value="Neko prezime">
-						</div>
-						<div class="divElementBox1">
-							<label for="ime">Poštanski broj</label>
-							<input id="ptt" name="ptt" type="text" value="" placeholder="Unesite poštanski broj">
-							<p>Provjerite svoj unos</p>
-						</div>
-						<div class="divElementBox1">
-							<label for="lozinka">Lozinka</label>
-							<input id="lozinka" name="lozinka" type="password" value="">
-						</div>
-						<div class="divElementBox1">
-							<label for="email">Email</label>
-							<input id="email" name="email" type="email" value="">
-						</div>
-						<div class="divElementBox2">
-							<input id="hobi1" name="hobi1" type="checkbox" value="Nogomet">
-							<label for="hobi1">Nogomet</label>
-						</div>
-						<div class="divElementBox2">
-							<input id="hobi2" name="hobi2" type="checkbox" value="Rukomet">
-							<label for="hobi2">Rukomet</label>
-						</div>
-						<div class="divElementBox2">
-							<input id="hobi3" name="hobi3" type="checkbox" value="Tenis">
-							<label for="hobi3">Tenis</label>
-						</div>
-						<div class="divElementBox2">
-							<input id="spol1" name="spol" type="radio" value="m">
-							<label for="spol1">Muški</label>
-						</div>
-						<div class="divElementBox2">
-							<input id="spol2" name="spol" type="radio" value="ž">
-							<label for="spol2">Ženski</label>
-						</div>
-						<div class="divElementBox3">
-							<label for="poruka">Vaša poruka</label>
-							<textarea id="poruka" name="poruka">Ovdje je value</textarea>
-						</div>
-						<div class="divElementBox3">
-							<label for="smjer">Vaš smjer</label>
-							<select id="smjer" name="smjer">
-								<option value="W34">Web dizajn</option>
-								<option>Grafički dizajn</option>
-								<option>Mobile programiranje</option>
-								<option>Java programiranje</option>
-							</select>
-						</div>
-						<div class="divElementBox4">
-							<input id="btt1" name="btt1" type="submit" value="Spremi">
-							<input id="btt2" name="btt2" type="reset" value="Obriši">
-						</div>
-					</form>
-				</div>
-				<script type="text/javascript">
-					$(document).ready(function(){
-						$('#ptt').keyup(function(){
-							var txt = $('#ptt').val();
-							var duzinaTxt = txt.length;
-							if(duzinaTxt<5){
-								$('#ptt').addClass('errorStyle1');
-								$('.divElementBox1 p').addClass('errorStyle1');
-							}else{
-								$('#ptt').removeClass('errorStyle1');
-								$('.divElementBox1 p').removeClass('errorStyle1');
-							}
-							if(duzinaTxt==5){
-								$('#ptt').attr('maxlength','5');
-							}
-							//console.log(txt);
-						});
-						$('.divGallery img').hover(function(){
-							var staraPutanja = $(this).attr('src');
-							var novaPutanja = $(this).attr('novaputanja');
-							 $(this).attr('src',novaPutanja);
-							 $(this).attr('novaputanja',staraPutanja);
-						});
-					}); // end ready
-					
-				</script>
-			</div>
-			<div class="divCol6 last">
-				<img src="images/about-img.jpg">
-			</div>
-			<div style="clear: both;"></div>
-		</div><!-- end #divSection2 -->
-	</div><!-- end #divSection2Wrap -->
 	<div id="divFooterWrap">
 		<div id="divFooter">
 			<div class="divCol3">
